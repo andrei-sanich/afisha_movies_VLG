8ca8bf5195eca516f5594a774b228c86a6eef6ce
andrei-sanich <andrew89s@mail.ru>
2018-08-26 12:29:11 +0300

    remove extra characters

--- static/js/get_movies.js
+++ static/js/get_movies.js
@@ -1,6 +1,5 @@
         function get_movies() {
             $.ajax({
-                type: "POST",
                 url: "/get_movies",
                 data: $('form').serialize(),
                 type: 'POST',
@@ -13,10 +12,10 @@
                         movie_data += '<td>'+value.name_eng+'</td>';
                         movie_data += '<td>'+value.name_rus+'</td>';
                         movie_data += '<td>'+value.year+'</td>';
-                        movie_data += '<td>'+value.genres+'</td>';
-                        movie_data += '<td>'+value.countries+'</td>';
-                        movie_data += '<td>'+value.directors+'</td>';
-                        movie_data += '<td>'+value.writers+'</td>';
+                        movie_data += '<td>'+value.genres.slice(1, -1)+'</td>';
+                        movie_data += '<td>'+value.countries.slice(1, -1)+'</td>';
+                        movie_data += '<td>'+value.directors.replace(/[{}""]/g, "")+'</td>';
+                        movie_data += '<td>'+value.writers.replace(/[{}""]/g, "")+'</td>';
                         movie_data += '<td>'+value.rus_prem+'</td>';
                         movie_data += '<td><a href='+value.url+'data-popup-info="enabled">Go to Kinopoisk.ru</a></td>';
                         movie_data += '</tr>';
--- view.py
+++ view.py
@@ -1,14 +1,13 @@
 from app import app
 from flask import render_template, request, json
 
-
+from models import Movie
 from parse_list import parse_list
-from models import Movieaaa
 
 
 @app.route('/')
 def index():
-    list_genres = [row[0] for row in Movieaaa.query.with_entities(Movieaaa.genres)]
+    list_genres = [row[0] for row in Movie.query.with_entities(Movie.genres)]
     genres = set()
     for genre in list_genres:
         temp = [g for g in parse_list(genre)]
@@ -20,10 +19,11 @@ def index():
 @app.route('/get_movies', methods=['GET', 'POST'])
 def get_movies():
     genres = request.form.getlist('genres')
-    all_movies = Movieaaa.query.all()
+    all_movies = Movie.query.all()
     find_movies = [movie for movie in all_movies if set(genres).issubset(set(parse_list(movie.genres)))]
     result = [res.to_json() for res in find_movies]
     return json.dumps({'movies': result})
+
 
 @app.route('/analytics')
 def analytics():
